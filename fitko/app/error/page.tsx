55f0066f7c3258528d3d04e6cce2f9fa76dcb746
ViceElez <vice.elezovic12@gmail.com>
2026-02-16 00:06:05 +0100

    initial setup

---
+++ fitko/app/error/page.tsx
@@ -0,0 +1,8 @@
+export default function ErrorPage(){
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">An error occurred</h1>
+            <p className="text-lg text-gray-600">Sorry, something went wrong. Please try again later.</p>
+        </div>
+    )
+}
